f72e5a2d908546298bb9e2620f863ee521f669de
Elvis Mack Jr <63875873+Bloo-561@users.noreply.github.com>
2021-10-17 16:22:00 -0400

    Added Base AFGC Website

---
+++ src/modules/my/background/background.js
@@ -0,0 +1,10 @@
+import { LightningElement} from 'lwc';
+
+export default class Background extends LightningElement {
+    songStatus = true;
+
+    muteSong() {
+        this.songStatus = !this.songStatus; 
+        console.log('Song Status Change');
+    }
+}
