98c7d60eb582eb87cc47ea13e432d60ff82ec13c
Elvis Mack Jr <63875873+Bloo-561@users.noreply.github.com>
2021-10-18 19:04:52 -0400

    Added some LWC compatiablity

--- src/modules/my/background/background.js
+++ src/modules/my/background/background.js
@@ -1,10 +1,12 @@
 import { LightningElement} from 'lwc';
 
 export default class Background extends LightningElement {
+
     songStatus = true;
 
     muteSong() {
         this.songStatus = !this.songStatus; 
         console.log('Song Status Change');
     }
+
 }
